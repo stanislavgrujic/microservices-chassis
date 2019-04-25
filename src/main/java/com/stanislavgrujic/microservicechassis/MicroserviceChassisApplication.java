1024e79c48b094f7df6a4084da5bc59b4dfbc332
stanislavgrujic <sgrujic@execom.eu>
2019-04-25 11:51:36 +0200

    Initiall commit

---
+++ src/main/java/com/stanislavgrujic/microservicechassis/MicroserviceChassisApplication.java
@@ -0,0 +1,13 @@
+package com.stanislavgrujic.microservicechassis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceChassisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceChassisApplication.class, args);
+	}
+
+}
